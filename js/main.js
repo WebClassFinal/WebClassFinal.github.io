8dec59481d586050cf0be5f0418aa87971fe4db9
Travis-Xiao <travis.xiao@gmail.com>
2014-07-12 13:33:46 +0800

    canvas height

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ var body, canvas, ctx;
 
 $(document).ready(function() {
     body = $('body');
+    body.tap(function () {
+        console.log("body tapped");
+    });
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     canvas.width = screen.width;
-    canvas.height = screen.height - 2 + 'px';
+    canvas.height = screen.height - 4;
 
-    body.tap(function () {
-        console.log("body tapped");
-        alert("body");
-    });
+
 });
