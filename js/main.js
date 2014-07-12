1e4265f6cb8978dc415cf998a8828a9c66e13391
Travis-Xiao <travis.xiao@gmail.com>
2014-07-12 13:18:57 +0800

    rename project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by Travis on 2014/7/12.
  */
+var body, canvas, ctx;
+
+$(document).ready(function() {
+    body = $('body');
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = screen.width;
+    canvas.height = screen.height - 2 + 'px';
+
+    body.tap(function () {
+        console.log("body tapped");
+        alert("body");
+    });
+});
