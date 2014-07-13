e173d6f08ae64cab82e548ff20b72a1f30b695d9
ZhangJiayu <zhangjiayu55@163.com>
2014-07-13 16:08:03 +0800

    The next step is to add medicine and bomb

--- js/map.js
+++ js/map.js
@@ -1,6 +1,12 @@
+/** 
+* Created by Paul on 2014/7/13
+*/
+
 const blank = 0,
     stone = 1,
-    medicine = 2;
+    grass = 2,
+    medicine = 3,
+    bomb = 4;
 
 function generateMap(size, preMap) {
 	var currentPreMap = preMap;
@@ -11,24 +17,24 @@ function generateMap(size, preMap) {
         mapContent: []
     };
     for(var i = 0 ; i < size.length ; i++){
-    	var tempOneLine;
-    	while(true){
-    		tempOneLine = randomOneLine(height);
-    		if(couldConnect(currentPreMap, tempOneLine)){
-    			break;
-    		}
-    	}
+        var tempOneLine;
+        if(currentPreMap.length == 0 || Math.floor(Math.random() * 3) > 0){
+    	   tempOneLine = randomOneLine(height, currentPreMap);
+        }
+        else{console.log(currentPreMap);
+            tempOneLine = {
+                height: height,
+                length: 1,
+                mapContent: [currentPreMap.mapContent[currentPreMap.length - 1].slice()]
+            }
+        }
     	newMap = connectMap(newMap, tempOneLine);
     	currentPreMap = tempOneLine;
     }
     return newMap;
 }
 
-function getElementAt(map, x, y) {
-    return (map.mapContent)[y][x];
-}
-
-function randomOneLine(height) {
+function randomOneLine(height, currentPreMap) {
     var caseNum = 3;
     var currentCase = Math.floor(Math.random() * 3);
     var map = {
@@ -36,30 +42,35 @@ function randomOneLine(height) {
         length: 1,
         mapContent: [new Array(height)]
     };
-    for (var i = 0; i < height; i++) {
-        (map.mapContent)[0][i] = blank;
+    while(true){
+        for (var i = 0; i < height; i++) {
+            (map.mapContent)[0][i] = blank;
+        }
+        switch (currentCase) {
+            case 0:
+                var stoneNum = Math.floor(Math.random() * height / 3 * 2);
+                for (var i = 0; i < stoneNum; i++) {
+                    (map.mapContent)[0][i] = stone;
+                }
+                break;
+            case 1:
+                var stoneStart = Math.floor(Math.random() * height / 3);
+                var stoneEnd = Math.floor(Math.random() * height / 3 * 2);
+                for (var i = stoneStart; i < stoneEnd; i++) {
+                    (map.mapContent)[0][i] = stone;
+                }
+                break;
+            case 2:
+                var stonePosition1 = Math.floor(Math.random() * height);
+                var stonePosition2 = Math.floor(Math.random() * height);
+                (map.mapContent)[0][stonePosition1] = stone;
+                (map.mapContent)[0][stonePosition2] = stone;
+                break;
+        }
+        if(couldConnect(currentPreMap, map)){
+            return map;
+        }
     }
-    switch (currentCase) {
-        case 0:
-            var stoneNum = Math.floor(Math.random() * height / 3 * 2);
-            for (var i = 0; i < stoneNum; i++) {
-                (map.mapContent)[0][i] = stone;
-            }
-            break;
-        case 1:
-            var stoneNum = Math.floor(Math.random() * height / 3 * 2);
-            for (var i = 0; i < stoneNum; i++) {
-                (map.mapContent)[0][height - 1 - i] = stone;
-            }
-            break;
-        case 2:
-            var stonePosition1 = Math.floor(Math.random() * height);
-            var stonePosition2 = Math.floor(Math.random() * height);
-            (map.mapContent)[0][stonePosition1] = stone;
-            (map.mapContent)[0][stonePosition2] = stone;
-            break;
-    }
-    return map;
 }
 
 function couldConnect(map1, map2) {
@@ -69,15 +80,57 @@ function couldConnect(map1, map2) {
 
     var map1End = map1.mapContent[map1.length - 1];
     var map2Start = map2.mapContent[0];
+console.log(map1End);console.log(map2Start);
+    var tempConnect = [map1End.slice(), map2Start.slice()];
     var height = map1.height;
-    var isValidConnect = false;
+    var isValidConnect = true;
 
-    for (var i = 0; i < height; i++) {
-        if (map1End[i] == blank && map2Start[i] == blank) {
-            isValidConnect = true;
-            break;
+    for(var i = 0 ; i < tempConnect.length ; i++){
+        for(var j = 0 ; j < height ; j++){
+            if(tempConnect[i][j] == blank || tempConnect[i][j] == medicine){
+                tempConnect[i][j] = 0;
+            }
+            else{
+                tempConnect[i][j] = 1;
+            }
         }
     }
+
+    function markBlank(xPosition, yPosition){
+        if(tempConnect[1 - xPosition][yPosition] == 0){
+            tempConnect[1 - xPosition][yPosition] = -1;
+            markBlank(1 - xPosition, yPosition);
+        }
+        if(yPosition > 0 && tempConnect[xPosition][yPosition - 1] == 0){
+            tempConnect[xPosition][yPosition - 1] = -1;
+            markBlank(xPosition, yPosition - 1);
+        }
+        if(yPosition < height - 1 && tempConnect[xPosition][yPosition + 1] == 0){
+            tempConnect[xPosition][yPosition + 1] = -1;
+            markBlank(xPosition, yPosition + 1);
+        }
+    }
+
+    (function(){
+        for(var i = 0 ; i < tempConnect.length ; i++){
+            for(var j = 0 ; j < height ; j++){
+                if(tempConnect[i][j] == 0){
+                    tempConnect[i][j] = -1;
+                    markBlank(i, j);
+                    return;
+                }
+            }
+        }
+    })();
+
+    for(var i = 0 ; i < tempConnect.length ; i++){
+        for(var j = 0 ; j < height ; j++){
+            if(tempConnect[i][j] == 0){
+                isValidConnect = false;
+            }
+        }
+    }
+    
     return isValidConnect;
 }
 
@@ -91,10 +144,10 @@ function connectMap(map1, map2){
     return newMap;
 }
 
-function drawImage(map, canvas){
+/*function drawImage(map, canvas){
     canvasContext = canvas.getContext("2d");
     canvasContext.rotate(Math.PI / 2 * (1));
-    canvasContext.scale(1, -1);
+    canvasContext.scale(1,-1);
     var heightPerBlock = canvas.width / map.height;
     var img = new Image;
     img.onload = function(){
@@ -108,17 +161,17 @@ function drawImage(map, canvas){
             }
         }
     }
-    img.src = "./images/stone.jpg";
-}
-//var newMap = {
-//        height: 10,
-//        length: 0,
-//        mapContent: []
-//    };
+    img.src = "./img/stone.jpg";   
+}*/
+/*var newMap = {
+        height: 10,
+        length: 0,
+        mapContent: []
+    };
 
-//newMap = generateMap({height: 10, length: 100}, newMap);
-//var canvas = $("#map");
-//canvas.parent().css({"left": "95px"});
-//canvas.attr("width", canvas.parent().width());
-//canvas.attr("height", 2 * canvas.parent().height());
-//drawImage(newMap, canvas.get(0));
+newMap = generateMap({height: 10, length: 100}, newMap);*/
+/*var canvas = $("#map");
+canvas.parent().css({"left": "95px"});
+canvas.attr("width", canvas.parent().width());
+canvas.attr("height", 2 * canvas.parent().height());
+drawImage(newMap, canvas.get(0));*/
